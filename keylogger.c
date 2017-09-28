3a2e708ae8096b70e126f71a22859fd8d564db1d
changmin.oh <chmn.oh@gmail.com>
2017-09-29 01:19:19 +0900

    dirty hack for ESC key to change IME mode into english

--- keylogger.c
+++ keylogger.c
@@ -19,35 +19,6 @@ int main(int argc, const char *argv[]) {
     CFRunLoopAddSource(CFRunLoopGetCurrent(), runLoopSource, kCFRunLoopCommonModes);
     CGEventTapEnable(eventTap, true);
 
-
-    // Clear the logfile if clear argument used or log to specific file if given.
-    if(argc == 2) {
-        if(strcmp(argv[1], "clear") == 0) {
-            fopen(logfileLocation, "w");
-            printf("%s cleared.\n", logfileLocation);
-            fflush(stdout);
-            exit(1);
-        } else {
-            logfileLocation = argv[1];
-        }
-    }
-
-    // Get the current time and open the logfile.
-    time_t result = time(NULL);
-    logfile = fopen(logfileLocation, "a");
-
-    if (!logfile) {
-        fprintf(stderr, "ERROR: Unable to open log file. Ensure that you have the proper permissions.\n");
-        exit(1);
-    }
-
-    // Output to logfile.
-    fprintf(logfile, "\n\nKeylogging has begun.\n%s\n", asctime(localtime(&result)));
-    fflush(logfile);
-
-    // Display the location of the logfile and start the loop.
-    printf("Logging to: %s\n", logfileLocation);
-    fflush(stdout);
     CFRunLoopRun();
 
     return 0;
@@ -60,9 +31,14 @@ CGEventRef CGEventCallback(CGEventTapProxy proxy, CGEventType type, CGEventRef e
     // Retrieve the incoming keycode.
     CGKeyCode keyCode = (CGKeyCode) CGEventGetIntegerValueField(event, kCGKeyboardEventKeycode);
 
-    // Print the human readable key to the logfile.
-    fprintf(logfile, "%s", convertKeyCode(keyCode));
-    fflush(logfile);
+//    // Print the human readable key
+//    fprintf(stdout, "%d: %s %d (down:%d up:%d changed:%d)\n", keycode, convertkeycode(keycode), type, kcgeventkeydown, kcgeventkeyup, kcgeventflagschanged);
+//    fflush(stdout);
+
+	// dirty hack for ESC keycode to reset IME to english mode.
+	if (type==kCGEventKeyDown && keyCode==53) {
+		system("issw com.apple.keylayout.ABC >/dev/null 2>&1");
+	}
 
     return event;
 }
